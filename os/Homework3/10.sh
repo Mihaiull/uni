4f110bdd4c342c2d57c03e526e9859950fa0e30a
Mihaiull <116212492+Mihaiull@users.noreply.github.com>
2025-04-13 22:38:38 +0300

    just don't use awk, life would be a lot better...

--- os/Homework3/10.sh
+++ os/Homework3/10.sh
@@ -3,28 +3,60 @@
 # Write a shell script which takes as parameters several file names.
 # The script will display the number of files, the average number of words per file and the total word count.
 
-# Initialize variables
-total_files=0
-total_words=0
+# # Initialize variables
+# total_files=0
+# total_words=0
 
-for file in "$@"; do
-    # Check if the file exists
-    if [[ -f "$file" ]]; then
-        # Increment the file count
-        total_files=$((total_files + 1))
-        # Get the word count for the file
-        total_words=$((total_words + $(wc -w < $file))) #why does this end up being 132 insteand of 15?
-    else
-        # Print an error message if the file does not exist
-        echo "File $file does not exist."
-    fi
-done
+# for file in "$@"; do
+#     # Check if the file exists
+#     if [[ -f "$file" ]]; then
+#         # Increment the file count
+#         total_files=$((total_files + 1))
+#         # Get the word count for the file
+#         total_words=$((total_words + $(wc -w < $file))) #why does this end up being 132 insteand of 15?
+#     else
+#         # Print an error message if the file does not exist
+#         echo "File $file does not exist."
+#     fi
+# done
 
-echo "Total files: $total_files"
-echo "Total words: $total_words"
-if [[ $total_files -gt 0 ]]; then
-    average_words=$((total_words / total_files))
-    echo "Average words per file: $average_words"
-else
-    echo "No valid files provided."
+# echo "Total files: $total_files"
+# echo "Total words: $total_words"
+# if [[ $total_files -gt 0 ]]; then
+#     average_words=$((total_words / total_files))
+#     echo "Average words per file: $average_words"
+# else
+#     echo "No valid files provided."
+# fi
+
+# unfortunately i am obligated to use awk
+
+# Check for at least one file argument
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 file1 [file2 ... fileN]"
+    exit 1
 fi
+
+# Pass all files to awk
+awk '
+BEGIN {
+    total_words = 0;
+    file_count = 0;
+}
+FNR == 1 { # First line of a new file (used to count files since you only get to the first line once)
+    file_count++; # New file started
+}
+{
+    total_words += NF; # NF = number of fields (words) in the current line
+}
+END {
+    if (file_count == 0) {
+        avg = 0;
+    } else {
+        avg = total_words / file_count;
+    }
+    printf "Number of files: %d\n", file_count;
+    printf "Average words per file: %.2f\n", avg;
+    printf "Total word count: %d\n", total_words;
+}
+' "$@" # Pass all files to awk
--- os/Homework3/13.sh
+++ os/Homework3/13.sh
@@ -4,34 +4,76 @@
 # The script will display the usernames and the total number of users that were connected to the server on that date and within that time frame
 # using awk
 
-month=$1
-day=$2
-time_interval=$3
+# FUCK THIS SHIT
 
-#get start time and end time
-start_time=$(echo "$time_interval" | cut -d '-' -f 1)
-end_time=$(echo "$time_interval" | cut -d '-' -f 2)
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <Month> <Day> <HH.MM-HH.MM>"
+    exit 1
+fi
 
-#i got the exact format that last prints so now it's just down to checking collumns
+MONTH=$1
+DAY=$2
+RANGE=$3
 
+START_HOUR=${RANGE%-*}
+END_HOUR=${RANGE#*-}
 
-echo $(last | awk -v month="$month" -v day="$day" -v start_time="$start_time" -v end_time="$end_time" '
+# Convert to HH:MM
+START_TIME=$(echo "$START_HOUR" | awk -F. '{printf("%02d:%02d", $1, $2)}') # -F specifies . as field separator - needed for splitting start_hour & end_hour into hour and minute components
+END_TIME=$(echo "$END_HOUR" | awk -F. '{printf("%02d:%02d", $1, $2)}') # the ugly ass %02d is just the way printf formats numbers as a 2 digit number with leading 0s if needed
+
+last | awk -v month="$MONTH" -v day="$DAY" -v start="$START_TIME" -v end="$END_TIME" '
+function time_to_minutes(t) {   #easier to just make a function
+    split(t, parts, ":")    #simple, split parts using : as separator
+    return parts[1]*60 + parts[2]
+}
+
 {
-    #check for the month and day (month on the 3rd column and day number on the 6th) this breaks when reading a system boot entry... I don t really like awk
-    if ($3 == month && $6 == day) {
-        #check for the time interval (time on the 4th column)
-        split($4, time, ":")
-        start_time_split = split(start_time, start_time_arr, ".")
-        end_time_split = split(end_time, end_time_arr, ".")
+    if ($0 ~ month " " day) {   ## if month and day match the pattern in $0 (the current line)
+        user = $1
 
-        #convert to minutes
-        start_time_minutes = start_time_arr[1] * 60 + start_time_arr[2]
-        end_time_minutes = end_time_arr[1] * 60 + end_time_arr[2]
-        time_minutes = time[1] * 60 + time[2]
+        # Extract login and logout times
+        match($0, /[A-Z][a-z]{2} [ 0-9]{1,2} ([0-9]{2}:[0-9]{2}) - ([0-9]{2}:[0-9]{2}|still logged in)/, times) #i gave up un the fucking columns since last has so many varying columns its so fucking shit fuck everything
+        #regex never fails me
+        #[A-Z][a-z]{2} - Matches three letters starting with capital letter (month abbreviation) - Ex: Jan, Feb, Mar
+        #[ 0-9]{1,2} - Matches one or two digits (day number) with optional space in front # Ex: 3 or  1 or 12
+        #([0-9]{2}:[0-9]{2}|still logged in)/ - # Matches either a time in HH:MM format or the text "still logged in"
+        # times - is an array holding the matched text:
+        # times[0] - the full match
+        # times[1] - login time
+        # times[2] - logout time
 
-        if (time_minutes >= start_time_minutes && time_minutes <= end_time_minutes) {
-            print $1
-            count++
+        if (times[1] == "") next    #obviously
+
+        login_time = times[1]
+        logout_time = times[2]
+
+        login_min = time_to_minutes(login_time)
+        start_min = time_to_minutes(start)
+        end_min = time_to_minutes(end)
+
+        if (logout_time == "still logged in") {
+            logout_min = 1440  # assume end of day (1440 is not a display resolution, its 24*60)
+        } else {
+            logout_min = time_to_minutes(logout_time)
+        }
+
+        # Overlapping time intervals
+        if ((login_min <= end_min) && (logout_min >= start_min)) {
+            users[user]++   #users is an associative array, awk automatically creates it if it doesnt exist, macar atat
+            total++
         }
     }
-}')
+}
+
+END {   #END block is executed after all input lines have been processed
+    print "Users connected in given interval:"
+    for (u in users) {
+        print u
+    }
+    print "Total number of connections: " total
+}
+'
+
+# sorry not sorry but this is not a problem to give to absolute begginers, even I (which I am not some big shit but I like to think I know my way around this stuff) struggled with it
+# conclusion: the awk collumns are unreliable as fuck and we should just all learn regex fucking waste of time
